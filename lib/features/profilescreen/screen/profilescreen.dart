894a5c77058af2a082d05cbaad04bea68275892e
ahmedelsapagh <TopBusiness110>
2023-11-23 10:54:56 +0200

    all done el mazoon version 1

--- lib/features/profilescreen/screen/profilescreen.dart
+++ lib/features/profilescreen/screen/profilescreen.dart
@@ -371,10 +371,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                     paddingHorizontal: getSize(context) / 20,
                                     borderRadius: getSize(context) / 24,
                                     onClick: () {
-                                      // errorGetBar('working_on_it'.tr());
-
-                                      Navigator.pushNamed(context,
-                                          Routes.selectMonthPlanPayment);
+                                      errorGetBar('working_on_it'.tr());
+                                      // Navigator.pushNamed(context,
+                                      //     Routes.selectMonthPlanPayment);
 
                                       ///renew
                                     }),
